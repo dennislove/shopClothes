0d34460d1c374ba20ee59c1094015256d3b69604
Trần Văn Tĩnh <116895926+dennislove@users.noreply.github.com>
2023-04-26 13:42:02 +0700

    sửa upload lần 2

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -1,28 +1,20 @@
-const form = document.getElementById('product-form');
+const form = document.getElementsByClassName('product-form');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  const name = document.getElementById('product-name').value;
-  const quantity = document.getElementById('product-quantity').value;
-  const type = document.getElementById('product-type').value;
-  const IDtype = document.getElementById('product-id-type').value;
+  const name = document.getElementsByClassName('.product-name').value;
+  const quantity = document.getElementsByClassName('.product-quantity').value;
+  const type = document.getElementsByClassName('.product-type').value;
+  const IDtype = document.getElementsByClassName('.product-id-type').value;
 
-  const OLDprice = document.getElementById('product-price-old').value;
-  const Newprice = document.getElementById('product-price-new').value;
-  
-  const description = document.getElementById('product-description').value;
-  const image = document.getElementById('product-image').files[0];
+  const OLDprice = document.getElementsByClassName('.product-price-old').value;
+  const Newprice = document.getElementsByClassName('.product-price-new').value;
+
+  const description = document.getElementsByClassName('.product-description').value;
+  const image = document.getElementsByClassName('.product-image').files[0];
 
 });
 
-function addTag(tag) {
-  var input = document.getElementById("tags-input");
-  var value = input.value;
-  if (value.length > 0) {
-    value += ", ";
-  }
-  value += tag;
-  input.value = value;
-}
+
 
 
